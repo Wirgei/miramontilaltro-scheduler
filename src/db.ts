107e996de8a41c25e1fa9ba7512ef1a11ebe999d
Wirgei <wirgei@yahoo.it>
2024-10-07 16:52:33 +0200

    Refactor query function to accept generic type and update return type

--- src/db.ts
+++ src/db.ts
@@ -4,13 +4,10 @@ import * as Mysql from 'mysql2/promise';
 
 let pool: Mysql.Pool | undefined;
 
-export let query = async (sql: string, values?: any | any[] | { [param: string]: any }) => {
-
+export let query = async <T>(sql: string, values?: any[]): Promise<[T[], Mysql.FieldPacket[]]> => {
   try {
-
     if (!pool) pool = Mysql.createPool(config.mysqlSynology as Mysql.PoolOptions);
-
-    return await pool.query(sql, values);
+    return await pool.query(sql, values) as [T[], Mysql.FieldPacket[]];
   } catch (err) {
     console.log(`Query code: ${sql}`);
     console.log(err);
